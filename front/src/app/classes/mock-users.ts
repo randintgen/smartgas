02d559022159d7b2b5ce9918824710bd8924f2fa
alexkafiris <alexkafiris@gmail.com>
2018-12-21 12:01:29 +0200

    deleted front

--- front/src/app/classes/mock-users.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user';
-
-export const Users: User[] = [
-    {username: 'user1', password: '1', email: 'user1@1.com'},
-    {username: 'user2', password: '2', email: 'user2@2.com'},
-    {username: 'user3', password: '3', email: 'user3@3.com'},
-    {username: 'user4', password: '4', email: 'user4@4.com'},
-    {username: 'user5', password: '5', email: 'user5@5.com'}
-];
