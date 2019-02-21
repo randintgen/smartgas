85528e01a0f4fea7112b1c11b07e96984427b75c
manzar96 <manoszar96@gmail.com>
2019-02-21 18:39:15 +0200

    Create shop Model created

---
+++ back/app/model/Shops/createShopModel.js
@@ -0,0 +1,63 @@
+'user strict';
+
+const sql = require('../db.js');
+
+var Shop = function(shop) {
+
+	this.name = shop.name;
+
+	this.tags = shop.tags;
+	this.withdrawn = shop.withdrawn;
+	this.imgpath = shop.ipath;
+  this.lng = shop.lng;
+  this.lat = shop.lat;
+  this.address = shop.address;
+
+};
+
+var newShop = function(id,shop) {
+	this.id = id;
+	this.name = shop.name;
+	this.tags = shop.tags;
+	this.withdrawn = shop.withdrawn;
+  this.lng = shop.lng;
+  this.lat = shop.lat;
+  this.address = shop.address;
+};
+
+Shop.create_shop = function(newshop,result) {
+
+	var temp,tagsplit;
+  console.log(newshop);
+	sql.query("INSERT INTO shops set ?", newshop, function (err, res) {
+
+		if(err) {
+
+			console.log("error in new shop : ", err);
+	        	result(true,{"success":false,"message":"Something went wrong,please try again later !"});
+		}
+
+		else {
+	        	console.log("New shop created , shopid -->  " + res.insertId );
+			var newpr = new newShop(res.insertId,newshop);
+			//console.log(newpr.id);
+
+			newpr.id = ""+newpr.id;
+
+			if(newpr.withdrawn==0) newpr.withdrawn=false
+			else newpr.withdrawn=true
+
+			tagsplit = newpr.tags.split(',');
+			temp = [];
+			for(var j=0 ; j<tagsplit.length ; j++){
+				temp.push(tagsplit[j]);
+			}
+			newpr.tags = temp;
+	        	result(null, {"success":true,"shop":newpr});
+
+		}
+
+	});
+};
+
+module.exports= Shop;
