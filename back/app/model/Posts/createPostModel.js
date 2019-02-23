cb30f2709c4a6d90d3721b3f6fcbc90c2f1ed8bb
Emmanuel Vasilakis <39127083+manolisvasilakis@users.noreply.github.com>
2019-02-23 04:21:11 +0200

    Add files via upload

---
+++ back/app/model/Posts/createPostModel.js
@@ -0,0 +1,60 @@
+'user strict';
+
+const sql = require('../db.js');
+
+//Task object constructor
+var Post = function(post) {
+    this.price = post.price;
+    this.dateFrom = post.dateFrom;
+    this.dateTo = post.dateTo;
+    this.productId = post.productId;
+    this.shopId = post.shopId;
+};
+
+Post.createpost = function createPost(newPost, userid, result) {
+
+    sql.query("SELECT fuelid, tags, type, description FROM fuel WHERE fuelid = ? LIMIT 1", newPost.productId, function (err, res) {
+        if (!res[0]) {
+            console.log("Fuel not found!");
+            result(true, {"success": false, "message": "Fuel with corresponding productId not found !"});
+        }
+        else {
+            sql.query("SELECT shopid, name, address, tags FROM shops WHERE shopid = ? LIMIT 1", newPost.shopId, function (err2, res2) {
+                if (!res2[0]) {
+                    console.log("Shop not found!");
+                    result(true, {"success": false, "message": "Shop with corresponding shopId not found !"});
+                }
+                else {
+                    inp = "INSERT INTO post (shopid, userid, fuelid, price, dateFrom, dateTo) VALUES ( '" + newPost.shopId + "' , '" + userid + "' , '" + newPost.productId + "' , '" + newPost.price + "' , '" + newPost.dateFrom + "' , '" + newPost.dateTo + "');";
+                    sql.query(inp, function (err3, res3) {
+                        if (err3) {
+                            console.log("error on insert : ", err3);
+                            result(true, {"success": false, "message": "Insert failed !"});
+                        }
+                        else {
+                            console.log("INSERT OK");
+                            result(null, {
+                                "success": true,
+                                "postId": res3.insertId,
+                                "price": newPost.price,
+                                "dateFrom": newPost.dateFrom,
+                                "dateTo": newPost.dateTo,
+                                "productName": res[0].type + ' ' + res[0].description,
+                                "productId": newPost.fuelid,
+                                "productTags": res[0].tags.split(','),
+                                "shopId": newPost.shopId,
+                                "shopName": res2[0].name,
+                                "shopTags": res2[0].tags.split(','),
+                                "shopAddress": res2[0].address
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+};
+
+
+module.exports= Post;
---
+++ back/app/model/Posts/deletePostModel.js
@@ -0,0 +1,23 @@
+'user strict';
+
+const sql = require('../db.js');
+
+del_post = function(postId, usrid, result){
+
+        // admin can delete anything ????
+        // if
+        var inp = "DELETE FROM post WHERE ( userid = '" + usrid + "' AND postid = '" + postId + "');";
+        sql.query(inp, function(err, res) {
+            if (err) {
+                console.log("ERROR ON DELETE POST :", err);
+                result(null, {"success":false,"message":"Delete failed !"}); // add sthhh
+            }
+            else if (res.affectedRows == 0) result(null, {"success":false,"message":"Please provide a valid postId !"});
+            else {
+                console.log("DELETED POST");
+                result(null, {"success":true,"message":"Your post has been deleted successfully!"});
+            }
+        });
+};
+
+module.exports= del_post;
