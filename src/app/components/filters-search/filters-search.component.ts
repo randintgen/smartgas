1fbac44baaacfab66005ea0d067c0496748fbc90
Constantinos <ckarouzos@gmail.com>
2019-02-25 19:30:55 +0200

    side bar

--- src/app/components/filters-search/filters-search.component.ts
+++ src/app/components/filters-search/filters-search.component.ts
@@ -1,38 +1,58 @@
-import { OnInit, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { OnInit, Component } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+export interface Types {
+  value: string;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-filters-search',
   templateUrl: './filters-search.component.html',
   styleUrls: ['./filters-search.component.css']
 })
-export class FiltersSearchComponent implements OnInit, OnDestroy {
-  mobileQuery: MediaQueryList;
 
-  fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
 
-  fillerContent = Array.from({length: 50}, () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
 
-  private _mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
-
+export class FiltersSearchComponent implements OnInit {
+ 
   ngOnInit() {
   }
 
+  options1: FormGroup;
+  options2: FormGroup;
+
+
+  constructor(fb: FormBuilder) {
+    this.options1 = fb.group({
+      hideLoc: false,
+      floatLabel: 'auto',
+    });
+   
+  }
+
+  disabled = false;
+  disabled2 = false;
+
+    types: Types[] = [
+      {value: 'type-0', viewValue: 'Βενζίνη'},
+      {value: 'type-1', viewValue: 'Πετρέλαιο'},
+      {value: 'type-2', viewValue: 'Φ. Αέριο'}
+    ]
+
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+
 }
+
