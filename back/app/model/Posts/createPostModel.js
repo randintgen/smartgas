bfc7719f3c98c158e2f0c2cb6d25e76017b7f3ea
Emmanuel Vasilakis <39127083+manolisvasilakis@users.noreply.github.com>
2019-02-28 04:42:27 +0200

    Update createPostModel.js

--- back/app/model/Posts/createPostModel.js
+++ back/app/model/Posts/createPostModel.js
@@ -1,5 +1,3 @@
-// should fix issue here or with trigger
-
 'user strict';
 
 const sql = require('../db.js');
@@ -7,32 +5,40 @@ const sql = require('../db.js');
 createpost = function createPost(newPost, userid, result) {
 
     sql.query("SELECT fuelid, tags, type, description FROM fuel WHERE fuelid = ? LIMIT 1", newPost.productId, function (err, res) {
-        if (!res[0]) {
-            console.log("Fuel not found!");
+        if (err) {
+            //console.log("error on insert : ", err);
+            result(true, {"success": false, "message": "Something went wrong while checking productId !"});
+        }
+        else if (!res[0]) {
+            //console.log("Fuel not found!");
             result(true, {"success": false, "message": "Fuel with corresponding productId not found !"});
         }
         else {
             sql.query("SELECT shopid, name, address, tags FROM shops WHERE shopid = ? LIMIT 1", newPost.shopId, function (err2, res2) {
-                if (!res2[0]) {
-                    console.log("Shop not found!");
+                if (err2) {
+                    //console.log("error on insert : ", err2);
+                    result(true, {"success": false, "message": "Something went wrong while checking shopId !"});
+                }
+                else if (!res2[0]) {
+                    //console.log("Shop not found!");
                     result(true, {"success": false, "message": "Shop with corresponding shopId not found !"});
                 }
                 else {
                     inp = "INSERT INTO post (shopid, userid, fuelid, price, dateFrom, dateTo) VALUES ( '" + newPost.shopId + "' , '" + userid + "' , '" + newPost.productId + "' , '" + newPost.price + "' , '" + newPost.dateFrom + "' , '" + newPost.dateTo + "');";
                     sql.query(inp, function (err3, res3) {
                         if (err3) {
-                            console.log("error on insert : ", err3);
+                            //console.log("error on insert : ", err3);
                             result(true, {"success": false, "message": "Insert failed !"});
                         }
                         else {
-                            console.log("INSERT OK");
+                            //console.log("INSERT OK");
                             result(null, {
                                 "success": true,
-                                "postId": res3.insertId,    // not sure if need postId
+                                "postId": res3.insertId,
                                 "price": newPost.price,
                                 "dateFrom": newPost.dateFrom,
                                 "dateTo": newPost.dateTo,
-                                "productName": res[0].type + ' ' + res[0].description,
+                                "productName": res[0].type + ' ' + res[0].description,  // prosoch edo
                                 "productId": newPost.fuelid,
                                 "productTags": res[0].tags.split(','),
                                 "shopId": newPost.shopId,
