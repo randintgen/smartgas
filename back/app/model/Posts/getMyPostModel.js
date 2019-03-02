524ee17e0f14759a65816af908c250f83f0394d6
Emmanuel Vasilakis <39127083+manolisvasilakis@users.noreply.github.com>
2019-03-02 06:20:36 +0200

    Add files via upload

---
+++ back/app/model/Posts/getMyPostModel.js
@@ -0,0 +1,31 @@
+'user strict';
+
+const sql = require('../db.js');
+
+MyPostlist = function(usrid, result) {
+
+    var f1 = "SELECT post.price, post.my_date as date, fuel.type, fuel.description, fuel.fuelid as productId, fuel.tags as productTags, shops.shopid as shopId, shops.name as shopName, shops.tags as shopTags, shops.address as shopAddress, shops.lng as shopLng, shops.lat as shopLat";
+    var fr = " FROM ((post INNER JOIN fuel ON fuel.fuelid = post.fuelid) INNER JOIN shops ON shops.shopid = post.shopid)";
+    f1 += fr + " ORDER BY date DESC;";
+    //console.log(f1);
+    sql.query(f1, function (err, res) {
+
+        if (err) {
+            //console.log("ERROR ON SEARCH : " + err);
+            result(true, {"success": false, "message": "Something went wrong,please try again later !"});
+        }
+        else {
+        //console.log(res);
+            var prices = [];
+            for(var i=0 ; i<res.length ; i++) {
+                res[i].productTags = res[i].productTags.split(',');
+                res[i].shopTags = res[i].shopTags.split(',');
+                res[i].date = JSON.stringify(res[i].date).substr(1,10);
+                prices.push(res[i]);
+            }
+            result(null, {"success": true, "count": prices.length, "prices": prices});
+        }
+    });
+}
+
+module.exports = MyPostlist;
