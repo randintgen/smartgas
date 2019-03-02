2cc05e09366f64ebc0f228680aa40a2eca9369dd
Emmanuel Vasilakis <39127083+manolisvasilakis@users.noreply.github.com>
2019-03-02 05:57:17 +0200

    Update getShopidModel.js

--- back/app/model/Shops/getShopidModel.js
+++ back/app/model/Shops/getShopidModel.js
@@ -6,35 +6,30 @@ get_pid = function(shopid,result) {
 
 	var final,tagsplit,temp;
 	//console.log(fuelid);
-	final = "SELECT shopid as id ,name,address,lng,lat,tags,withdrawn FROM shops WHERE shopid="+shopid+" LIMIT 1 ;";
+	final = "SELECT shopid as id, name, address, lng, lat, tags, withdrawn FROM shops WHERE shopid = " + shopid + " LIMIT 1 ;";
 	sql.query(final,function (err, res) {
 
 		if(err) {
-
-			console.log("error: ", err);
-			result(true,{"success":false,"message":"Something went wrong,please try again later !"});
+			//console.log("error: ", err);
+			result(true, {"success":false,"message":"Something went wrong,please try again later !"});
 		}
 		else {
 			//console.log(res);
 
-			if(!res[0]) result(null,{"success":false,"message":"No matching shop with the id given !"});
+			if(!res[0]) result(null,{"success":false,"message": "No matching shop with the id given !"});
 			else {
 
-				res[0].id = ""+res[0].id;
+				res[0].id = "" + res[0].id;
 
-				if(res[0].withdrawn==0) res[0].withdrawn=false
-				else res[0].withdrawn=true
+				if (res[0].withdrawn == 0) res[0].withdrawn = false;
+				else res[0].withdrawn = true;
 
-				tagsplit = res[0].tags.split(',');
-				temp = [];
-				for(var j=0 ; j<tagsplit.length ; j++){
-					temp.push(tagsplit[j]);
-				}
-				res[0].tags = temp;
-				result(null,{"success":true,"product":res[0]});
+				res[0].tags = res[0].tags.split(',');
+
+				result(null,{"success":true,"shop":res[0]});
 			}
 		}
 	});
 };
 
-module.exports= get_pid;
+module.exports = get_pid;
