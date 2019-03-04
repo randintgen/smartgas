d44100cd9112048c2d18b54268798c5ac65983a0
Constantinos <ckarouzos@gmail.com>
2019-03-04 14:40:29 +0200

    photos

--- front/src/app/components/about-us/about-us.component.ts
+++ front/src/app/components/about-us/about-us.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  private KANAVAKIS = "https://imgur.com/jVzErYI.jpg";
+  private KAFIRIS = "https://imgur.com/F2maxMB.jpg";
+  private ALEXAKIS = "https://i.imgur.com/IaCBMbo.jpg";
+  private ZARANIS = "https://imgur.com/8OBJaCt.jpg";
+  private VASILAKIS = "https://i.imgur.com/4QlRffm.jpg";
+  private KAROUZOS = "https://imgur.com/Xb5rgvF.jpg";
   constructor() { }
 
   ngOnInit() {
